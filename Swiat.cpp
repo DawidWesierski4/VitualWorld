de3054ea8d0c0c7a549a25dbcd69b78472fdd8a1
dawidW4 <dawidwesierski4@gmail.com>
2023-01-06 12:34:06 +0100

    add Swiat class prototype

--- Swiat.cpp
+++ Swiat.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+class Swiat {
+
+public:
+    void WykonajTure(void)
+    {
+        std::cout<<"WykonajTure"<<std::endl;
+    }
+
+    void rysujSwiat(void)
+    {
+        std::cout<<"rysujSwiat"<<std::endl;
+    }
+};
+
 int main()
 {
+    Swiat virtualWorld;
     std::cout<<"hello world"<<std::endl;
+    virtualWorld.rysujSwiat();
+    virtualWorld.WykonajTure();
+    return 0;
 }
