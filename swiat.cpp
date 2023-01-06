ba0b02d11ec2a66a523bc29d6188203f921e6360
DawidWesierski4 <dawidwesierski4@gmail.com>
2023-01-06 14:28:07 +0100

    add rTerminal function

--- swiat.cpp
+++ swiat.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include "swiat.h"
 
-class Swiat {
+/**
+ * reset Terminal
+ *  Function should reset/clear the terminal so that you don't write lines one
+ *  after another. Instead you overwrite what was written before
+ *  idk how this works but does the job on linux <3 happy googling folks
+*/
+inline void rTerminal(void)
+{
+    std::cout<<"\033[2J"<<std::flush;
+}
+
+class Swiat
+{
+    /* It is impossible to change the name of the author cuz it is a const */
+    const char* const imieAutora = "Dawid Wesierski";
+    const char* const indeksAutora = "180029";
+    int **swiatArray;
+    int sizeSwiatArray;
 
 public:
     void WykonajTure(void)
@@ -9,17 +26,47 @@ public:
         std::cout<<"WykonajTure"<<std::endl;
     }
 
-    void rysujSwiat(void)
+    void rysujSwiat(int x)
     {
+        int i;
+
+    
+        swiatArray = new int*[x];
+        for (i = 0; i < x; i++)
+            swiatArray[i] = new int[x];
+
         std::cout<<"rysujSwiat"<<std::endl;
+        std::cout<<"imie Autora = "<<imieAutora<<std::endl;
+        std::cout<<"indeks Autora = "<<indeksAutora<<std::endl;
     }
+
+    ~Swiat()
+    {
+        int i;
+        for (i = 0; i < sizeSwiatArray; i++)
+            delete[] swiatArray[i];
+
+        delete[] swiatArray;
+    }
+};
+
+class Organizm
+{
+public:
+    virtual int sila() = 0;
+    virtual int inicjatywa() = 0;
+    virtual void polozenie(int x, int y) = 0;
+    virtual Swiat swiat() = 0;
 };
 
 int main()
 {
+    bool reset;
     Swiat virtualWorld;
     std::cout<<"hello world"<<std::endl;
-    virtualWorld.rysujSwiat();
+    virtualWorld.rysujSwiat(10);
     virtualWorld.WykonajTure();
+    rTerminal();
+    virtualWorld.rysujSwiat(10);
     return 0;
 }
--- swiat.h
+++ swiat.h
@@ -2,5 +2,8 @@
 #define SWIAT_H
 
 class Swiat;
+class Organizm;
+
+void rTerminal(void);
 
 #endif /* SWIAT_H header guard */
