f2152fd6cb1b08c65cd2ccf399b56add92a1652f
DawidWesierski4 <dawidwesierski4@gmail.com>
2023-01-06 16:04:54 +0100

    add prototype of rysujSwiat function

--- swiat.cpp
+++ swiat.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "swiat.h"
 
+
+#define BLACK_BLOCK 0xE2
+
 /**
  * reset Terminal
  *  Function should reset/clear the terminal so that you don't write lines one
@@ -15,31 +18,49 @@ inline void rTerminal(void)
 class Swiat
 {
     /* It is impossible to change the name of the author cuz it is a const */
-    const char* const imieAutora = "Dawid Wesierski";
+    const char* const imieAutora = "Dawid Węsierski";
     const char* const indeksAutora = "180029";
-    int **swiatArray;
+    Organizm ***swiatArray;
     int sizeSwiatArray;
 
 public:
     Swiat(int x)
     {
         int i;
-        swiatArray = new int*[x];
+        this->sizeSwiatArray = i;
+        swiatArray = new Organizm**[x];
         for (i = 0; i < x; i++)
-            swiatArray[i] = new int[x];
+        {
+            this->swiatArray[i] = new Organizm*[x];
+
+            for(i = 0; i < x; i++)
+                this->swiatArray[i] == NULL;
+        }
     }
 
     void WykonajTure(void)
     {
         std::cout<<"WykonajTure"<<std::endl;
+
     }
 
     void rysujSwiat()
     {
-        
+        int i, j;
+        std::cout<<"Rysujemy Świat"<<std::endl;
+        for (i = 0; i < this->sizeSwiatArray; i++ )
+        {
+            for (j = 0; j < this->sizeSwiatArray; j++)
+            {
+            if (!this->sizeSwiatArray)
+                std::cout<<BLACK_BLOCK;
+            else
+                std::cout<<this->swiatArray[i][j]->symbol;
+            }
 
 
-        std::cout<<"rysujSwiat"<<std::endl;
+        }
+        rTerminal();
         std::cout<<"imie Autora = "<<imieAutora<<std::endl;
         std::cout<<"indeks Autora = "<<indeksAutora<<std::endl;
     }
@@ -57,6 +78,8 @@ public:
 class Organizm
 {
 public:
+    char symbol;
+    Swiat* ourSwiat;
     virtual int sila() = 0;
     virtual int inicjatywa() = 0;
     virtual void polozenie(int x, int y) = 0;
@@ -67,9 +90,9 @@ class Zwierze : public Organizm
 {
     int sila;
     int inicjatywa;
-    int symbol;
 
 public:
+    int symbol;
     Zwierze(char smb)
     {
         this->symbol = smb;
@@ -81,11 +104,8 @@ public:
 int main()
 {
     bool reset;
-    Swiat virtualWorld;
+    Swiat virtualWorld(10);
     std::cout<<"hello world"<<std::endl;
-    virtualWorld.rysujSwiat(10);
-    virtualWorld.WykonajTure();
-    rTerminal();
-    virtualWorld.rysujSwiat(10);
+    virtualWorld.rysujSwiat();
     return 0;
 }
